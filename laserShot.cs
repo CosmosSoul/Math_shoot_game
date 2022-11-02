d656a7f85a0e9a0083398274da842fb42c6917d7
Cosmos_ <ka0snBa21@gmail.com>
2022-11-02 10:28:24 +0900

    Shot collision now properly increases score

--- laserShot.cs
+++ laserShot.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class laserShot : MonoBehaviour
 {
+
+    //public playerController playerController;
+    //public gameManager gameManager;
+    public scoreScript scoreScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //playerController = GameObject.Find("Player Controller").GetComponent<playerController>();
+        //gameManager = GameObject.Find("Game Manager").GetComponent<gameManager>();
+        scoreScript = GameObject.Find("Game Manager").GetComponent<scoreScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.CompareTag("enemy")) || (collision.gameObject.CompareTag("obstacle")))
+        {
+            Debug.Log("One " + collision.gameObject.tag + " has hit you, captain!");
+            // scoreScript.scoreValue += 10;
+            scoreScript.Test();
+            scoreScript.AddPoint();
+            Debug.Log(scoreScript.scoreValue);
+
+        }
     }
 }
