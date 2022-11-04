0ab15087cdfd4d2a013d313068be26d0c48bda85
Cosmos_ <ka0snBa21@gmail.com>
2022-11-04 09:45:12 +0900

    Laser shot and obstacle are now destroyed on collision

--- laserShot.cs
+++ laserShot.cs
@@ -10,7 +10,6 @@ public class laserShot : MonoBehaviour
     public scoreScript scoreScript;
     public float deathDelay = 2f;
 
-
     // Start is called before the first frame update
     void Start()
     {
