13d604af27dc682c1b29347b3c9d9f112ed513a5
Cosmos_ <ka0snBa21@gmail.com>
2022-12-16 12:22:37 +0900

    Enemy health value properly randomizes and displays above enemy

--- enemyController.cs
+++ enemyController.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 public class enemyController : MonoBehaviour
 {
 
-    public int enemyLives;
-    public TextMeshProUGUI enemyLivesText;
+    //public int enemyLife = 5;
+    public int enemyMaxHealth = 5;
+    public int enemyCurrentHealth;
+    public TextMeshProUGUI enemyLifeText;
+
+    public GameObject enemyLife;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //enemyLifeText.text = randomEnemyLife.ToString();
+        enemyCurrentHealth = Random.Range(-enemyMaxHealth, enemyMaxHealth);
+        enemyLifeText.text = "" + enemyCurrentHealth;
     }
 
     // Update is called once per frame
